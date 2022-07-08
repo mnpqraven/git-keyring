c778988eedc0ab68a49a973ff72400c45583f504
Othi <mnpq.raven@gmail.com>
2022-07-09 02:15:16 +0700

    Update git-install.sh

--- git-install.sh
+++ git-install.sh
@@ -33,3 +33,4 @@ git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 git config --global user.name and user.email "$email"
 xclip $HOME/.ssh/id_ed25519.pub
 sudo systemctl enable sshd.service
+cp $HOME/git-keyring/.profile $HOME/
