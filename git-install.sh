88e3a81aa0f3923595356cf1f304f27c1a172e58
Othi <mnpq.raven@gmail.com>
2022-07-09 00:31:06 +0700

    Update git-install.sh

--- git-install.sh
+++ git-install.sh
@@ -2,6 +2,12 @@
 
 sudo pacman -Syu
 sudo pacman -S neovim gnome-keyring libsecret
+
+cd $HOME
+#vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       
 git clone https://github.com/mnpqraven/git-keyring.git
 cd $HOME/git-keyring
 # cp git-keyring/.gitconfig ./
