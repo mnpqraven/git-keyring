467b3d0ce6d72e8ccfe2fd0969718a9485138dd6
Othi <mnpq.raven@gmail.com>
2022-07-08 23:57:54 +0700

    Update git-install.sh

--- git-install.sh
+++ git-install.sh
@@ -4,7 +4,7 @@ sudo pacman -Syu
 sudo pacman -S neovim gnome-keyring libsecret
 
 cd $HOME
-cp git-keyring/.gitconfig ./
+# cp git-keyring/.gitconfig ./
 echo "do NOT sudo this, if you did it by accident, cancel"
 echo "enter your email:"
 while true;
@@ -20,9 +20,19 @@ esac
 done
 
 # cp $HOME/dotfiles/.gitconfig $HOME
-ssh-keygen -t ed25519 -C "$email"
+#ssh-keygen -t ed25519 -C "$email"
+ssh-keygen
 eval "$(ssh-agent -s)"
 ssh-add -L
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 echo "open up new term and xclip the keys, theres a sha key and a ed key"
 sudo systemctl enable sshd.service
+
+# test neovim install
+mkdir .config
+mkdir .config/nvim
+touch .config/nvim/init.vim
+echo 'plug#begin()' > .config/nvim/init.vim
+echo 'Plug \'tpope/vim-fugitive\'' > .config/nvim/init.vim
+echo 'plug#end()' > .config/nvim/init.vim
+echo 'tell who you are with git config --global user.name and user.email'
